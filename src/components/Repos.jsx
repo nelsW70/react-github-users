72dd7f4055b5a379d936b1410f2f0a4787ef99f6
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-27 10:11:30 -0400

    add: first chart

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -7,7 +7,7 @@ const Repos = () => {
   const { repos } = useContext(GithubContext);
   console.log(repos);
 
-  return <h2>repos component</h2>;
+  return <ExampleChart />;
 };
 
 const Wrapper = styled.div`
