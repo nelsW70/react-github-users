228e3d732626967a08917ae07922584a0cfee3c9
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-27 10:59:14 -0400

    add: calculate most used language

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -5,7 +5,30 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
   const { repos } = useContext(GithubContext);
-  console.log(repos);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1
+      };
+    }
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  console.log(languages);
 
   const chartData = [
     {
@@ -25,7 +48,7 @@ const Repos = () => {
   return (
     <section className="global-section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   );
