0e8c7d8c81d33fb8ad80f55237e660d57d6cfe06
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-29 14:24:09 -0400

    update: charts

--- src/components/Charts/Pie3D.jsx
+++ src/components/Charts/Pie3D.jsx
@@ -21,15 +21,30 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
-    type: 'pie3d', // The chart type
+    type: 'pie2d', // The chart type
     width: '100%', // Width of the chart
-    height: '400', // Height of the chart
+    height: '350', // Height of the chart
     dataFormat: 'json', // Data type
     dataSource: {
       // Chart Configuration
       chart: {
         caption: 'Languages',
-        theme: 'fusion',
+        captionFontColor: '#102a42',
+        captionFontBold: 0,
+        captionFontSize: 20,
+        captionFont: 'Roboto',
+        baseFont: 'Open Sans',
+        baseFontSize: 16,
+        baseFontColor: '#617d98',
+        smartLineColor: '#617d98',
+        showShadow: 0,
+        showPlotBorder: 0,
+        paletteColors:
+          '#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA',
+        use3DLighting: 0,
+        useDataPlotColorForLabels: 0,
+        bgColor: '#FFFFFF',
+        showBorder: 0,
         decimals: 0,
         pieRadius: '45%'
       },
@@ -37,6 +52,7 @@ const ChartComponent = ({ data }) => {
       data
     }
   };
+
   return <ReactFC {...chartConfigs} />;
 };
 
